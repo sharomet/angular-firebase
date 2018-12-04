5e656449d0f9b953a99815062c4bdaa6af7f2f6b
sharomet <sharometal@gmail.com>
2018-12-04 16:09:06 +0200

    Created project. Included firebase

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  providers: [AngularFirestore],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCTfGoWngzc5z4ln0bAFz0FOseMmOdWhOc",
+        authDomain: "angular-firestore-b75a6.firebaseapp.com",
+        databaseURL: "https://angular-firestore-b75a6.firebaseio.com",
+        projectId: "angular-firestore-b75a6",
+        storageBucket: "angular-firestore-b75a6.appspot.com",
+        messagingSenderId: "62452500181"
+    }
+};
